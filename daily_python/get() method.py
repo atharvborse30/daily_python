0b0b350bd220727dbcad00d3f65f1187dd86dd54
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-09-19 00:00:08 +0530

    Add files via upload

---
+++ daily_python/get() method.py
@@ -0,0 +1,12 @@
+# create a dictionary
+my_dict = {'apple':3,'banana':5,'cherry':2}
+
+#Using .get() to retrieve values
+apple_count = my_dict.get('apple')
+banana_count = my_dict.get('banana')
+grape_count = my_dict.get('grape')
+
+#Printing the results
+print(apple_count)
+print(banana_count)
+print(grape_count)
